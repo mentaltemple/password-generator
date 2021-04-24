bf11c44126316b2b64bc0217e3259ab9e4b62256
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-24 00:55:13 -0600

    added readme file, added final array for password output

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -26,7 +26,34 @@ var lower = [
   "y",
   "z",
 ];
-// var upper = []; **CAN I AUTOMATE??
+var upper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = [
   "!",
@@ -57,11 +84,16 @@ var special = [
   "<",
   ">",
 ];
+
+var withLow = [];
+var withUpper = [];
+var withNum = [];
+var withChar = [];
+
+var passArray = [];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); //stores data from id #generate (Generate Password" button) into the object "generateBtn"
-
-var holdArray = [];
-var passArray = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -72,44 +104,67 @@ function writePassword() {
 }
 
 //writePassword,
-// Add event listener to generate button
-generateBtn.addEventListener("click", function lengthPrompt() {
-  var passLength = prompt(
-    "Password Generator: Please choose a password length between 8-128 characters"
-  );
-  if (passLength < 8 || passLength > 128) {
-    alert("Please choose a number between 8-128");
-    lengthPrompt();
-  } else {
-    var conLower = confirm("Would you like to include lowercase letters?");
-    var conUpper = confirm("Would you like to include UPPERCASE LETTERS?");
-    var conNumbers = confirm("Would you like to include numbers?");
-    var conChar = confirm(
-      "Would you like to include special characters? (!@#$%^&*+=)"
-    );
-  }
 
-  if (
-    conLower == false &&
-    conUpper == false &&
-    conNumbers == false &&
-    conChar == false
-  ) {
-    alert("Please select at least one character attribute!");
-    lengthPrompt();
-  } else alert("Thank you! Click the Generate Pasword button to display your new password");
-  if (conLower == true) {
-    holdArray = passArray.concat(lower);
+// Add event listener to generate button
+generateBtn.addEventListener(
+  "click",
+  function lengthPrompt() {
+    var passLength = prompt(
+      "Password Generator: Please choose a password length between 8-128 characters"
+    );
+    if (passLength < 8 || passLength > 128) {
+      alert("Please choose a number between 8-128");
+      lengthPrompt();
+    } else {
+      var conLower = confirm("Would you like to include lowercase letters?");
+      var conUpper = confirm("Would you like to include UPPERCASE LETTERS?");
+      var conNumbers = confirm("Would you like to include numbers?");
+      var conChar = confirm(
+        "Would you like to include special characters? (!@#$%^&*+=)"
+      );
+    }
+
+    if (
+      conLower == false &&
+      conUpper == false &&
+      conNumbers == false &&
+      conChar == false
+    ) {
+      alert("Please select at least one character attribute!");
+      lengthPrompt();
+    } else alert("Thank you! Your password will be generated below.");
+    if (conLower == true) {
+      withLow = passArray.concat(lower);
+    }
+    if (conUpper == true) {
+      withUpper = passArray.concat(upper);
+    }
+    if (conNumbers == true) {
+      withNum = passArray.concat(numbers);
+    }
+    if (conChar == true) {
+      withChar = passArray.concat(special);
+    }
+
+    var finalArray = withLow.concat(withUpper, withNum, withChar);
+    console.log(withLow);
+    console.log(withUpper);
+    console.log(withNum);
+    console.log(withChar);
+    console.log(finalArray);
+
+    // function passShuff() {
+    //   var i, j, k;
+    //   for (i = passLength; i > 0; i--) {
+    //     j = Math.floor(Math.random(finalArray) * i);
+    //     k = finalArray[i];
+    //     finalArray[i] = finalArray[j];
+    //     finalArray[j] = k;
   }
-  if (conUpper == true) {
-    holdArray = passArray.concat(lower);
-  }
-  if (conNumbers == true) {
-    holdArray = passArray.concat(numbers);
-  }
-  if (conChar == true) {
-    holdArray = passArray.concat(special);
-  }
-  console.log(holdArray);
-  console.log(passArray);
-});
+  // }
+
+  // passShuff();
+  // console.log(passShuff);
+
+  // writePassword();
+);
---
+++ README.md
@@ -0,0 +1,27 @@
+# <password-generator>
+
+## Description
+
+- My motivation behind this project was to deepen my understanding of JavaScript by building a functional web app.
+- I built this project because it was a complex puzzle to solve and it provided a lot of learning opportunities for JavaScript.
+- This project allows a user to generate a secure password incorporating lowercase, uppercase, numerical and special characters.
+- I learned a ton about functions, methods, objects, arrays, variables.
+- As of right now, it's not fully functional, but I learned a lot just getting to this point. I hope I can get it fully completed by the time you are reading this.
+
+## Installation
+
+Click on the link to take you to the deployed site: https://mentaltemple.github.io/password-generator/
+
+## Usage
+
+After navigating to the site, click the red "Generate Password" button. Follow the series of prompts and your password will be displayed.
+
+[Screenshot of Password Generator](Assets/images/passScrnShot.png)
+
+## Credits
+
+https://www.w3schools.com
+Alex Jurgs
+Lisa Barton
+TJ Courey
+Ben Machock
