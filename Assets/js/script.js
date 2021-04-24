6149fbc90905d546b10dfe20d90b44bd87f64ff8
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-23 20:16:01 -0600

    created arrays for numbers, letters and special characters, created function to prompt user with password criteria questions and redirect when necessary, concat method not functioning properly

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,107 @@
+var lower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+// var upper = []; **CAN I AUTOMATE??
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "{",
+  "}",
+  "[",
+  "]",
+  ";",
+  ":",
+  "~",
+  "+",
+  "=",
+  "-",
+  "`",
+];
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //stores data from id #generate (Generate Password" button) into the object "generateBtn"
+
+var holdArray = [];
+var passArray = [];
+
+function lengthPrompt() {
+  var passLength = prompt(
+    "Password Generator: Please choose a password length between 8-128 characters"
+  );
+  if (passLength < 8 || passLength > 128) {
+    alert("Please choose a number between 8-128");
+    lengthPrompt();
+  } else {
+    var conLower = confirm("Would you like to include lowercase letters?");
+    var conUpper = confirm("Would you like to include UPPERCASE LETTERS?");
+    var conNumbers = confirm("Would you like to include numbers?");
+    var conChar = confirm(
+      "Would you like to include special characters? (!@#$%^&*+=)"
+    );
+  }
+
+  console.log(passLength);
+
+  if (
+    conLower == false &&
+    conUpper == false &&
+    conNumbers == false &&
+    conChar == false
+  ) {
+    alert("Please select at least one character attribute!");
+    lengthPrompt();
+  } else
+    alert(
+      "Thank you! Click the Generate Pasword button to display your new password"
+    );
+  if (conLower == true) {
+    passArray.concat(lower);
+  }
+  if (conUpper == true) {
+    passArray.concat(lower);
+  }
+  if (conNumbers == true) {
+    passArray.concat(numbers);
+  }
+  if (conChar == true) {
+    passArray.concat(special);
+  }
+  console.log(holdArray);
+  console.log(passArray);
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +109,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //When (Generate Button) is clicked...
+
+lengthPrompt();
