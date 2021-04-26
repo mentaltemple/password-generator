749fa3aaa5da4fad0d0969e4c9787371ef03776a
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-26 15:12:36 -0600

    added function to randomize final password array, added function to return final password and display to user, reorganized and cleaned up code structure

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -103,68 +103,58 @@ function writePassword() {
   passwordText.value = password;
 }
 
-//writePassword,
-
 // Add event listener to generate button
-generateBtn.addEventListener(
-  "click",
-  function lengthPrompt() {
-    var passLength = prompt(
-      "Password Generator: Please choose a password length between 8-128 characters"
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passLength = prompt(
+    "Password Generator: Please choose a password length between 8-128 characters"
+  );
+  if (passLength < 8 || passLength > 128) {
+    alert("Please choose a number between 8-128");
+    lengthPrompt();
+  } else {
+    var conLower = confirm("Would you like to include lowercase letters?");
+    var conUpper = confirm("Would you like to include UPPERCASE LETTERS?");
+    var conNumbers = confirm("Would you like to include numbers?");
+    var conChar = confirm(
+      "Would you like to include special characters? (!@#$%^&*+=)"
     );
-    if (passLength < 8 || passLength > 128) {
-      alert("Please choose a number between 8-128");
-      lengthPrompt();
-    } else {
-      var conLower = confirm("Would you like to include lowercase letters?");
-      var conUpper = confirm("Would you like to include UPPERCASE LETTERS?");
-      var conNumbers = confirm("Would you like to include numbers?");
-      var conChar = confirm(
-        "Would you like to include special characters? (!@#$%^&*+=)"
-      );
-    }
-
-    if (
-      conLower == false &&
-      conUpper == false &&
-      conNumbers == false &&
-      conChar == false
-    ) {
-      alert("Please select at least one character attribute!");
-      lengthPrompt();
-    } else alert("Thank you! Your password will be generated below.");
-    if (conLower == true) {
-      withLow = passArray.concat(lower);
-    }
-    if (conUpper == true) {
-      withUpper = passArray.concat(upper);
-    }
-    if (conNumbers == true) {
-      withNum = passArray.concat(numbers);
-    }
-    if (conChar == true) {
-      withChar = passArray.concat(special);
-    }
-
-    var finalArray = withLow.concat(withUpper, withNum, withChar);
-    console.log(withLow);
-    console.log(withUpper);
-    console.log(withNum);
-    console.log(withChar);
-    console.log(finalArray);
-
-    // function passShuff() {
-    //   var i, j, k;
-    //   for (i = passLength; i > 0; i--) {
-    //     j = Math.floor(Math.random(finalArray) * i);
-    //     k = finalArray[i];
-    //     finalArray[i] = finalArray[j];
-    //     finalArray[j] = k;
   }
-  // }
 
-  // passShuff();
-  // console.log(passShuff);
+  if (
+    conLower == false &&
+    conUpper == false &&
+    conNumbers == false &&
+    conChar == false
+  ) {
+    alert("Please select at least one character attribute!");
+    lengthPrompt();
+  } else alert("Thank you! Your password will be generated below.");
+  if (conLower == true) {
+    passArray = passArray.concat(lower);
+  }
+  if (conUpper == true) {
+    passArray = passArray.concat(upper);
+  }
+  if (conNumbers == true) {
+    passArray = passArray.concat(numbers);
+  }
+  if (conChar == true) {
+    passArray = passArray.concat(special);
+  }
 
-  // writePassword();
-);
+  var finalPass = "";
+  function passShuff() {
+    var i, j, k;
+    for (i = passLength; i > 0; i--) {
+      j = Math.floor(Math.random() * passArray.length);
+      k = passArray[j];
+
+      finalPass = finalPass + k;
+    }
+  }
+
+  passShuff();
+  return finalPass;
+}
